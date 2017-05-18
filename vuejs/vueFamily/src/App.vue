542fc57a39a5c84404bf33bb3686d96488df8d79
huanglong <you@example.com>
2017-05-18 15:38:05 +0800

    add vue demo

---
+++ vuejs/vueFamily/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <!--img src="./assets/logo.png"-->
+    <!--h1></h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><a href="#" target="_blank">{{msg}}</a></li>
+          <firstcomponent></firstcomponent>
+    </ul-->
+    <!--ul><li><firstcomponent></firstcomponent></li></ul-->
+    <!--ul>
+        <li><router-link to="/first">点我跳转到第一页</router-link></li>
+        <li><router-link to="/second">点我跳转到第二页</router-link></li>
+      </ul-->
+    <div class="header">我是title</div>
+    <router-view class="view"></router-view>
+    <div class="footer">我是footer</div>
+  </div>
+</template>
+
+<script>
+// import firstcomponent from './component/firstcomponent.vue'
+// import secondcomponent from './component/secondcomponent.vue'
+// export default {
+//   name: 'app',
+//   data () {
+//     return {
+//       msg: 'Welcome to Your Vue.js App'
+//     }
+//   },
+//   components: { firstcomponent,secondcomponent }
+// }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.header{
+  font-size: 18px;
+  color: #349212;
+  margin-bottom: 30px;
+}
+.footer{
+  font-size: 18px;
+  color: blue;
+  margin-top: 30px;
+}
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}*/
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ vuejs/vueFamily/src/component/firstcomponent.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+<h2>我是第 1 个子页面</h2>
+<div v-show="firstItem">
+1+1 = 多少？
+<button class="sign"  @click="secondItemfunc">下一题</button>
+</div>
+<div v-show="secondItem">
+2+2 = 多少？
+<button class="sign"  @click="thirdItemfunc">下一题</button>
+</div>
+<div v-show="thirdItem">
+3+3 = 多少？
+<button class="sign"  @click="goResult">完成</button>
+</div>
+</div>	
+</template>
+<script type="text/javascript">
+export default {
+    data() {
+            return {
+                firstItem: true,
+                secondItem: false,
+                thirdItem: false
+            };
+        },
+        methods: {
+            goResult: function() {
+                this.$router.push("/second");
+            },
+            secondItemfunc: function() {
+                this.firstItem = false;
+                this.secondItem = true;
+            },
+            thirdItemfunc: function() {
+                this.secondItem = false;
+                this.thirdItem = true;
+            }
+        }
+}
+
+
+
+</script>
